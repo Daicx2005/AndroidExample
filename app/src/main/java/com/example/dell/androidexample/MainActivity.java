154beb63ddb78514cf81be8be3f0f3cc417650ad
Daicx <daicx2005@163.com>
2018-09-30 14:45:20 +0800

    secode I commit it.

--- app/src/main/java/com/example/dell/androidexample/MainActivity.java
+++ app/src/main/java/com/example/dell/androidexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.androidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.helloword)).setText("不喜欢");
     }
 
     @Override
